ea7e80be72c8550619b0e8422969d729abd87f71
hp <hp@RAVIBAGHEL>
2023-08-29 22:21:07 +0530

    Add project files.

---
+++ secondsmalestelementinthearray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 7, 3, 9, 2, 5 };
+        int smallest = int.MaxValue;
+        int secondSmallest = int.MaxValue;
+
+        foreach (int num in array)
+        {
+            if (num < smallest)
+            {
+                secondSmallest = smallest;
+                smallest = num;
+            }
+            else if (num < secondSmallest && num != smallest)
+            {
+                secondSmallest = num;
+            }
+        }
+
+        Console.WriteLine("Second smallest element: " + secondSmallest);
+    }
+}
